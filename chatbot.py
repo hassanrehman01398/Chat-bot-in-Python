6c2c13f7765111ad7b85a6f1c2af0d3152d7b80f
Muhammad Hassan Ur Rehman <47937749+hassanrehman01398@users.noreply.github.com>
2019-12-06 21:27:47 +0500

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 22 10:36:37 2019
+
+@author: hp
+"""
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+print("hassan")
